f9daef5b2d475caadeb13f1648edb7440cbdf4b0
DavidGamFlo <73413949+DavidGamFlo@users.noreply.github.com>
2021-08-26 02:55:36 -0500

    Actualizacion de comentarios
    Se agregaron al archivo Fibonacci.dart los comentarios pertinentes

--- Fibonacci.dart
+++ Fibonacci.dart
@@ -1,8 +1,8 @@
-//Fibonacci
+//Fibonacci David G
 void main() {
-  final n = 13;
-  var fibo = 0;
-  var aux1 = 1;
+  final n = 13; //Numeros de Fibonacci a obtener
+  var fibo = 0; //Variable de muestre del numero deseado
+  var aux1 = 1; //Variables de almacenamiento a sumar
   var aux2 = 0;
   for (var i = 0; i < n; i++) {
     fibo = aux1 + aux2;
